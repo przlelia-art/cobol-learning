0308e0a0e82e7b8ac18db04834481dd2fe798ac9
Lélia Perez <przlelia@gmail.com>
2026-03-18 14:24:45 +0100

    feat: add file exercise - write student names to a file

---
+++ fichiers/write.cbl
@@ -0,0 +1,31 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. EcrireFichier.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+           SELECT MON-FICHIER ASSIGN TO "eleves.txt"
+           ORGANIZATION IS LINE SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD MON-FICHIER.
+       01  WS-LIGNE PIC X(50).
+
+       WORKING-STORAGE SECTION.
+       01  WS-NOM PIC X(20).
+
+       PROCEDURE DIVISION.
+
+           OPEN OUTPUT MON-FICHIER
+           MOVE "Alexandre" TO WS-LIGNE 
+           WRITE WS-LIGNE
+           MOVE "Léa" TO WS-LIGNE
+           WRITE WS-LIGNE
+           MOVE "David" TO WS-LIGNE
+           WRITE WS-LIGNE
+           CLOSE MON-FICHIER 
+           DISPLAY "Fichier créé avec succès !"
+
+           STOP RUN.
